d5729e754fb49399840291c87246d5bd551324d5
lingli1 <lingli1@ebay.com>
2020-04-24 20:40:35 +0800

    update dockerfile

--- src/main/java/com/example/demojersey/common/UserSvcConfigBean.java
+++ src/main/java/com/example/demojersey/common/UserSvcConfigBean.java
@@ -1,42 +1,40 @@
 package com.example.demojersey.common;
 
-import com.example.demojersey.utils.CSVProcessorUtil;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Singleton;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class UserSvcConfigBean {
+    private static final Logger logger = Logger.getLogger(UserSvcConfigBean.class);
     private static UserSvcConfigBean userSvcConfigBean = new UserSvcConfigBean();
     private Properties commonConfigProperties = null;
-    private static final Logger logger = Logger.getLogger(UserSvcConfigBean.class);
 
     private UserSvcConfigBean() {
-        try {
-            commonConfigProperties = getConfigMapFromFile("src/main/resources/config/config.properties");
-        } catch (FileNotFoundException e) {
-            logger.error(e.getMessage(), e);
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-        }
+//        try {
+////            InputStream in = UserSvcConfigBean.class.getResourceAsStream("/config/config.properties");
+////            FileInputStream fin = (FileInputStream)in;
+////            System.out.println("Path: " + UserSvcConfigBean.class.getResource("/config/config.properties").getPath());
+//
+//            logger.info("Path: " + UserSvcConfigBean.class.getResource("/config/config.properties").getPath());
+//            commonConfigProperties = getConfigMapFromFile( UserSvcConfigBean.class.getResource("/config/config.properties").getPath());
+//        } catch (FileNotFoundException e) {
+//            logger.error(e.getMessage(), e);
+//        } catch (IOException e) {
+//            logger.error(e.getMessage(), e);
+//        }
     }
 
     public static UserSvcConfigBean getInstance( ) {
         return userSvcConfigBean;
     }
 
-    public String getPropertyByName(String propertyName) {
-        return commonConfigProperties.getProperty(propertyName);
-    }
-
     public String getCSVRootFilePath() {
-        return getPropertyByName("csv.files.path");
+//        return commonConfigProperties.getProperty("csv.files.path");
+//        return "/root/demo/";
+        return "/tmp/demo/";
     }
 
     public static Properties getConfigMapFromFile(String propertiesFile) throws IOException {
